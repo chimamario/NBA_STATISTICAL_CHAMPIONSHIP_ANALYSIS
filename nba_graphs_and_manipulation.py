d02be9294dcc7764230ffb6a414ff8aa8d51ed5e
chimamario <mariochima646@gmail.com>
2024-07-11 19:57:35 -0600

    created fg percent graph

--- nba_graphs_and_manipulation.py
+++ nba_graphs_and_manipulation.py
@@ -1,16 +1,54 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df_players_playoff =  pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/nba_project_2024/cleaned_up_data and_code/real playoff 2023-2024 NBA Player Stats - Playoffs copy.csv")
 df_players_regular =  pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/nba_project_2024/cleaned_up_data and_code/nba_players_regular_season.csv")
 df_regular_teams = pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/nba_project_2024/cleaned_up_data and_code/regular_season_nba.csv")
 df_playoff_teams = pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/nba_project_2024/cleaned_up_data and_code/playoff_nba.csv")
 
-print(df_players_regular.head())
-print(df_players_playoff.head())
+# print(df_players_regular.head())
+# print(df_players_playoff.head())
 
-regular_players_above_fga_average = df_players_regular['fg'].mean()
-# playoff_players_above_fga_average = df_players_playoff['fg'].mean()
-print(regular_players_above_fga_average)
-# print(playoff_players_above_fga_average)
+#made the columns for both the playoff and regular seasons match
+playoff_columns = df_players_playoff.columns.tolist()
+df_players_regular.columns = playoff_columns
 
-#next task - change column in regular players to make with playoff players
+print(playoff_columns)
+
+#finding the field goal attempt average and filtering out all the players who averaged higher than the league average
+regular_players_above_fga_average = df_players_regular['FGA'].mean()
+playoff_players_above_fga_average = df_players_playoff['FGA'].mean()
+
+above_FGA_regular_players = df_players_regular.loc[df_players_regular['FGA'] > regular_players_above_fga_average]
+above_FGA_playoff_players = df_players_playoff.loc[df_players_playoff['FGA'] > playoff_players_above_fga_average]
+
+regular_field_goal_percent_average = above_FGA_regular_players['FG%'].mean()
+playoff_field_goal_percent_average = above_FGA_playoff_players['FG%'].mean()
+print(regular_field_goal_percent_average)
+
+
+#ploting the field goal results
+regular_field_goal_percent = above_FGA_regular_players['FG%'].to_list()
+playoff_field_goal_percent = above_FGA_playoff_players['FG%'].to_list()
+
+regular_x_values = range(len(regular_field_goal_percent))
+playoff_x_values = range(len(playoff_field_goal_percent))
+
+
+##scatter plots for data
+plt.scatter(regular_x_values, regular_field_goal_percent)
+plt.scatter(playoff_x_values, playoff_field_goal_percent)
+
+##respective field goal averages are also added to the plot
+plt.hlines(y = regular_field_goal_percent_average, xmin = 0, xmax = max(regular_x_values), color = 'r', linestyle = 'dashed', label = 'Regular Season Field Goal Average')
+plt.hlines(y = playoff_field_goal_percent_average, xmin = 0, xmax = max(playoff_x_values), color = 'g', linestyle = 'dashed', label = 'Playoffs Field Goal Average')
+
+##cleaning up plot
+plt.legend(['Regular Season', 'Playoffs','Regular Season Field Goal Average', 'Playoffs Field Goal Average' ])
+plt.xlabel('NBA Players With Above Average Field Goal Attempts')
+plt.ylabel('Field Goal Shooting Percentage')
+plt.title("Different in Field Goal Percentage Between the Regular Season and Playoffs")
+plt.show()
+
+
+#next task - find standard deviation for plot
