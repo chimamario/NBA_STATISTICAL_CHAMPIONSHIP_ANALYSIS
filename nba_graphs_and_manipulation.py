b5e967cd38176c900eaa4722558055db6254ff33
chimamario <mariochima646@gmail.com>
2024-07-15 20:30:10 -0600

    added info to point difference analysis

--- nba_graphs_and_manipulation.py
+++ nba_graphs_and_manipulation.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import tabulate as tabulate
 
 df_players_playoff =  pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/nba_project_2024/cleaned_up_data and_code/real playoff 2023-2024 NBA Player Stats - Playoffs copy.csv")
 df_players_regular =  pd.read_csv(r"/Users/mariochima/Desktop/my first folder/coding folder/nba_project_2024/cleaned_up_data and_code/nba_players_regular_season.csv")
@@ -63,18 +64,18 @@ playoff_fg_std = np.std(playoff_field_goal_percent)
 ####COMPARING PTS DIFFERENCE BETWEEN SEASONS
 
 #finding players who shot above the FGA
-point_diff_players = above_FGA_regular_players['Player'].tolist()
+point_diff_players = above_FGA_regular_players['Player'].tolist() #getting a list fo players that shot above the FGA
 
-pts_diff_regular_df = df_players_regular.loc[df_players_regular['Player'].isin(point_diff_players)]
+pts_diff_regular_df = df_players_regular.loc[df_players_regular['Player'].isin(point_diff_players)] #filtering the columns of players that shot above the FGA
 pts_diff_playoff_df = df_players_playoff.loc[df_players_playoff['Player'].isin(point_diff_players)]
 
-pts_diff_regular = pts_diff_regular_df['PTS']
+pts_diff_regular = pts_diff_regular_df['PTS'] #only selecting the PTS column
 pts_diff_playoff = pts_diff_playoff_df['PTS']
 
-pts_diff_regular_list = pts_diff_regular.tolist()
+pts_diff_regular_list = pts_diff_regular.tolist() #creating a list of the PTS column
 pts_diff_playoff_list = pts_diff_playoff.tolist()
 
-x_plot_values = range(0,len(pts_diff_regular))
+x_plot_values = range(0,len(pts_diff_regular)) #x plot values to plot PTS list
 x_plot_values_two = range(0,len(pts_diff_playoff))
 
 #creating dictionary to obtain specific values
@@ -89,12 +90,12 @@ plt.scatter(x_plot_values,pts_diff_playoff)
 ax.set_xticks(x_plot_values)
 ax.set_xticklabels(point_diff_players, rotation = 90)
 
-regressed_count = 0
+regressed_count = 0 #counting how many players regressed and improved in the playoffs
 progress_count = 0
-regressed_player_list = []
+regressed_player_list = [] #obtaining the index value for all the players based of if they regressed or improved
 progress_player_list = []
 
-#shows if players regressed or improved their PPG during the playoffs
+#shows if players regressed or improved their PPG during the playoffs (this is how they show the green or red dotted lines)
 for x in x_plot_values:
     if regular_pts_dict[x] > playoff_pts_dict[x]:
         regressed_count +=1
@@ -122,14 +123,13 @@ plt.clf()
 # print(regressed_player_list, progress_player_list)
 # print(point_diff_players)
 
-regressed_names = []
+regressed_names = [] #going thorugh regressed and improved lists (the ones with index values) and obtaining the players names
 progress_names = []
-
 for num in regressed_player_list:
     regressed_names.append(point_diff_players[num])
-
 for num in progress_player_list:
     progress_names.append(point_diff_players[num])
+
 
 regressed_avg_numerator = 0
 progress_avg_numerator = 0
@@ -137,14 +137,35 @@ regressed_name_ppg_differential_dict = {}
 progress_name_ppg_differential_dict = {}
 
 # print(progress_names)
-
+#merged playoff and regular season tables to create another table comparing the PPG values
 regular_playoff_ppg_diff_merged_df = pd.merge(pts_diff_regular_df, pts_diff_playoff_df, on = 'Player', suffixes=('_regular', '_playoffs'))
 regular_playoff_ppg_diff_merged_df['Point Difference'] = regular_playoff_ppg_diff_merged_df['PTS_playoffs'] - regular_playoff_ppg_diff_merged_df['PTS_regular']
-
 specific_columns = regular_playoff_ppg_diff_merged_df[['Player', 'PTS_regular', 'PTS_playoffs','Point Difference']]
 
 print(specific_columns)
+point_difference_mean = specific_columns['Point Difference'].mean()
+print(point_difference_mean)
 
+for player in specific_columns['Player']:
+    value = specific_columns.loc[specific_columns['Player'] == player, 'Point Difference']
+    value = float(value.iloc[0])
+    if  value > 0:
+        progress_avg_numerator += value
+    else:
+        regressed_avg_numerator += value
 
-# for player in regressed_names:
-# columns = pts_diff_regular.columns.tolist()
+regressed_avg = regressed_avg_numerator/len(regressed_names)
+progress_avg = progress_avg_numerator/len(progress_names)
+# print(regressed_avg_numerator)
+# print(progress_avg_numerator)
+# print(regressed_avg)
+# print(progress_avg)
+
+largest_point_difference = specific_columns.nlargest(5, 'Point Difference')
+smallest_point_difference = specific_columns.nsmallest(5, 'Point Difference')
+print(largest_point_difference)
+print(smallest_point_difference)
+
+specific_columns.to_csv('PPG_differential.csv')
+
+# print(tabulate.tabulate(specific_columns, headers = 'keys', tablefmt = 'psql'))
